e033d2a91046f476d64479ddee51ba8330c12cd4
Dexer <73297572+DexerBR@users.noreply.github.com>
2025-01-20 14:46:24 -0300

    remove unused env setup

--- builder/platforms/iossimulator.py
+++ builder/platforms/iossimulator.py
@@ -1,62 +1,10 @@
-import os
-
 from builder.platforms import execute_build
-from builder.utils import run_command
-from builder.versions import SKIA_VERSION
 
 SUPPORTED_ARCHITECTURES = ("arm64", "x64")
 
 
 def setup_env():
-    # Clone depot_tools
-    run_command(
-        [
-            "git",
-            "clone",
-            "https://chromium.googlesource.com/chromium/tools/depot_tools.git",
-        ],
-        "Cloning depot_tools",
-    )
-
-    # Verify Depot Tools Installation
-    run_command(
-        [os.path.join(os.getcwd(), "depot_tools", "gclient")],
-        "Verifying Depot Tools Installation",
-    )
-
-    # Clone Skia Repository and Fetch Dependencies
-    run_command(
-        ["git", "clone", "https://skia.googlesource.com/skia.git"],
-        "Cloning Skia Repository",
-    )
-
-    skia_path = os.path.join(os.getcwd(), "skia")
-
-    # Fetch and checkout to the specific branch (Chrome/SKIA_VERSION)
-    run_command(
-        ["git", "fetch", "-v"],
-        "Fetching Skia Repository",
-        cwd=skia_path,
-    )
-    run_command(
-        ["git", "checkout", f"origin/chrome/{SKIA_VERSION}"],
-        f"Checking out Chrome/{SKIA_VERSION} branch",
-        cwd=skia_path,
-    )
-
-    # Sync Skia dependencies using git-sync-deps script
-    run_command(
-        ["python3", "tools/git-sync-deps"],
-        "Syncing Skia Dependencies",
-        cwd=skia_path,
-    )
-
-    # Fetch Ninja binary for Skia
-    run_command(
-        ["python3", "bin/fetch-ninja"],
-        "Fetching Ninja for Skia",
-        cwd=skia_path,
-    )
+    pass
 
 
 def build(target_cpu, custom_build_args=None, override_build_args=None, archive_output=False):
